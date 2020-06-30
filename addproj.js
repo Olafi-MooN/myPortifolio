f6cc22861fc01cdf9c9b4b13406b919b7dc4392d
Olafi-MooN <alefmastertutor@gmail.com>
2020-06-30 19:29:48 -0300

    ok

--- addproj.js
+++ addproj.js
@@ -1,3 +1,3 @@
 exports.getProjetos = (req, res) =>  res.render('mainProjetos');
-exports.getAdd = (req, res) =>  res.render('addProjetos');
+exports.getAdd = (req, res) =>  res.render('addAbout');
 exports.getAbout = (req, res) => res.render('addAbout');
