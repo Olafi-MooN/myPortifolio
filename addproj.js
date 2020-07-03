0b6ca4dec34783e32f078b6b1cde13f092766abd
Olafi-MooN <alefmastertutor@gmail.com>
2020-07-02 22:45:46 -0300

    segurança

--- addproj.js
+++ addproj.js
@@ -4,27 +4,27 @@ exports.getProjetos = (req, res) =>  {
     if (confirmation.confirmation){
         return res.render('mainProjetos');
     }else{
-        return res.send('Você não realizou login');
+        return res.send('somente administradores');
     }
 }
 exports.getAdd = (req, res) =>  {
     if (confirmation.confirmation) {
         return res.render('addprojetos');
     }else{
-        return res.send('Você não realizou login');
+        return res.send('somente administradores');
     }
 }
 exports.getAbout = (req, res) => {
     if (confirmation.confirmation) {
         return res.render('addAbout');
     }else{
-        return res.send('Você não realizou login');
+        return res.send('somente administradores');
     }
 }
 exports.getLogin = (req, res) => {
     if (confirmation.confirmation) {
         return res.render('alterLogin');
     }else{
-        return res.send('Você não realizou login');
+        return res.send('somente administradores');
     }
 }
