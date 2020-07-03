845726b666f9a1abc56c1e4bf257b60039978e44
Olafi-MooN <alefmastertutor@gmail.com>
2020-07-02 22:35:17 -0300

    Segurança

--- addproj.js
+++ addproj.js
@@ -1,18 +1,30 @@
 const confirmation = require('./login');
 
 exports.getProjetos = (req, res) =>  {
-    if (confirmation.confirmation) return res.render('mainProjetos');
-    return res.send('Você não realizou login');
+    if (confirmation.confirmation){
+        return res.render('mainProjetos');
+    }else{
+        return res.send('Você não realizou login');
+    }
 }
 exports.getAdd = (req, res) =>  {
-    if (confirmation.confirmation) return res.render('adprojetos');
-    return res.send('Você não realizou login');
+    if (confirmation.confirmation) {
+        return res.render('addprojetos');
+    }else{
+        return res.send('Você não realizou login');
+    }
 }
 exports.getAbout = (req, res) => {
-    if (confirmation.confirmation) return res.render('addAbout');
-    return res.send('Você não realizou login');
+    if (confirmation.confirmation) {
+        return res.render('addAbout');
+    }else{
+        return res.send('Você não realizou login');
+    }
 }
 exports.getLogin = (req, res) => {
-    if (confirmation.confirmation) return res.render('alterLogin');
-    return res.send('Você não realizou login');
+    if (confirmation.confirmation) {
+        return res.render('alterLogin');
+    }else{
+        return res.send('Você não realizou login');
+    }
 }
